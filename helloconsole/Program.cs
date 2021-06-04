ba85c338310613c93b9bb54fde95087bca99d3aa
Cullen Hallman <63475504+SultanOfSloths@users.noreply.github.com>
2021-06-04 13:12:30 -0500

    first commit

---
+++ helloconsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace helloconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var input = Console.ReadLine();
+            
+            if (input.Equals("Cullen", StringComparison.OrdinalIgnoreCase))
+            {
+                var path = $"{Directory.GetCurrentDirectory()}/io.txt";
+                path = Path.GetFullPath(path);
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine($"Hello {input},");
+                    sw.WriteLine("Screw you!");
+                    sw.WriteLine("This is my file now!");
+                }
+
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        Console.Write((char)sr.Read());
+                    }
+                }
+            }
+        }
+    }
+}
